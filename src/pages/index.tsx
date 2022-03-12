095f7ca90c0a7273d6b96a3cda7bed25f1975237
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2022-03-12 18:42:38 -0300

    Title dinâmico por página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 import styles from '../styles/home.module.scss'
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello <span>World</span> 
+    <>
+      <Head>
+        <title> Início | ig.news</title>
+      </Head>
+
+      <h1 className={styles.title}>
+        Hello <span>World</span>
       </h1>
+    </>
   )
 }
