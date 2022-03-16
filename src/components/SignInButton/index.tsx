7fed2de85afbbcbc4bd2e8d83df1ed34a162b344
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2022-03-16 10:57:48 -0300

    static site generation

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -4,7 +4,7 @@ import {FiX} from 'react-icons/fi'
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const isUserLoggedIn = true;
   return isUserLoggedIn ? (
     <button type="button"
     className={styles.signInButton}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import {GetServerSideProps} from 'next'
+import {GetStaticProps} from 'next'
 import Head from 'next/head'
 import {stripe} from '../services/stripe'
 import { SubscribeButton } from '../components/SubscribeButton'
 
 import styles from './home.module.scss'
+
+// Client-side
+// Server-side
+// Static Site Generation
 
 interface HomeProps {
   product: {
@@ -37,7 +41,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps= async () => {
   const price = await stripe.prices.retrieve('price_1KdMErGyVztrkAhGuO0WOrdF')
   
   const product = {
@@ -51,7 +55,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60  * 24, // 24 hours
   }
 
 
