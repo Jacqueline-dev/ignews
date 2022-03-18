fc3c8e17726478ce26a0746da28a0096c5fb1419
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2022-03-18 16:27:55 -0300

    Autenticação Next Auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,28 @@
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
+import {signIn, useSession, SessionProvider} from 'next-auth/react'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button type="button"
+  const { data: session }  = useSession()
+ // const isUserLoggedIn = true;
+
+  return session ? (
+    <button 
+
+    type="button"
     className={styles.signInButton}
-    >
+ >
       <FaGithub color="#04d361"/>
       Jacqueline Ferreira
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
-
-    
-  ) : (
-    <button type="button"
+   ) : (
+    <button 
+    type="button"
     className={styles.signInButton}
+    onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417"/>
       Sign in with GitHub
