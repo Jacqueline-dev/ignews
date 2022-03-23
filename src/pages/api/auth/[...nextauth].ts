5f857c15abe80a8df8d302fcf164f6ef2e8177a3
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2022-03-23 19:38:46 -0300

    verificando usuario duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
-import {query as q} from 'faunadb'
+import { query as q } from 'faunadb'
 
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
 
 import { fauna } from '../../../services/fauna'
 
@@ -11,39 +11,33 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
-        params:{
-          scope:'read:user'
+        params: {
+          scope: 'read:user'
         }
       }
-      
-    }),
-    
+    })
   ],
 
- 
-
   callbacks: {
-    async signIn(user, account, profile) {
-      const { email} = user
-      
+    async signIn({user, account, profile}) {
+      const { email } = user
+
       try {
-      await fauna.query(
-        q.Create(
-          q.Collection('users'),
-
-          { data:{ email }}
-
-        
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
+          )
         )
-      )
-      return true
-    } catch {
-      return false
+        return true
+      } catch {
+        return false
+      }
     }
-
-
-    
-     
-    },
   }
 })
